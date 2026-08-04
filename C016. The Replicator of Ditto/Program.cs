e2410c2e75b6aec0e5e80bb5fd783a12d32063d8
alariash <alaricashwell@proton.me>
2026-08-04 22:13:47 +0200

    Fix the Replicator of Ditto

---
+++ C016. The Replicator of Ditto/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] array1 = new int[5];
+int[] array2 = new int[5];
+
+Console.WriteLine("Provide five numbers:");
+for (int i = 0; i < array1.Length; i++)
+{
+    Console.Write($"{i}: ");
+    array1[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 0; i < array1.Length; i++)
+{
+    array2[i] = array1[i];
+}
+
+// Display Results
+Console.WriteLine("First Array:");
+for (int i = 0; i < array1.Length; i++)
+{
+    Console.Write($"{array1[i]} ");
+}
+Console.WriteLine("\nSecond Array:");
+for (int i = 0; i < array2.Length; i++)
+{
+    Console.Write($"{array2[i]} ");
+}
