c3533a560c3818c5eaad62defe1c101f6f179801
alariash <alaricashwell@proton.me>
2026-08-04 22:24:49 +0200

    Fix the code for Freach [for a price]

---
+++ C017a. The Laws of Freach/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array = new int[] {4, 51, -7, 13, -99, 15, -8, 45, 90};
+
+int currentSmallest = int.MaxValue;
+foreach (var number in array)
+{
+    if (number < currentSmallest)
+        currentSmallest = number;
+}
+
+Console.WriteLine(currentSmallest);
---
+++ C017b. The Laws of Freach/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = new int[] {4, 51, -7, 13, -99, 15, -8, 45, 90};
+
+int total = 0;
+
+foreach (var number in array)
+{
+    total += number;
+}
+
+float average = (float)total / array.Length;
+Console.WriteLine(average);
