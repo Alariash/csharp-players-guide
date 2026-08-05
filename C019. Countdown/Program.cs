3524950a21eb03baf7a523ca66ee55d8a61c31dd
alariash <alaricashwell@proton.me>
2026-08-05 21:24:18 +0200

    Solve the "Countdown" challenge

---
+++ C019. Countdown/Program.cs
@@ -0,0 +1,11 @@
+﻿void CountdownFrom(int x)
+{
+    Console.WriteLine(x);
+    if (x == 1)
+        return;
+    else
+        CountdownFrom(x-1);
+}
+
+// It's the Final Countdown!
+CountdownFrom(10);
