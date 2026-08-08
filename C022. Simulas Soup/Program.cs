495888998f660da47b9a2febc8a7f030c266a006
alariash <alaricashwell@proton.me>
2026-08-08 21:59:34 +0200

    Solve the Simulas Soup challenge

--- C022. Simulas Soup/Program.cs
+++ C022. Simulas Soup/Program.cs
@@ -24,4 +24,4 @@ Console.WriteLine($"{dish.foodSeasoning} {dish.foodIngredient} {dish.foodType}")
 
 enum Type {Soup = 1, Stew = 2, Gumbo = 3};
 enum MainIngredient {Mushrooms = 1, Chicken = 2, Carrots = 3, Potatoes = 4};
-enum Seasoning {Spicy = 1, Salty = 2, Sweet = 3};
+enum Seasoning {Spicy = 1, Salty = 2, Sweet = 3}; 
