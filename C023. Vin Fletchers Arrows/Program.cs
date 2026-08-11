f7916506b553ab99aaca7e9e309efd2d1675c1f1
alariash <alaricashwell@proton.me>
2026-08-11 12:46:45 +0200

    Solve the Vin Fletcher's Arrows challenge

--- C023. Vin Fletchers Arrows/Program.cs
+++ C023. Vin Fletchers Arrows/Program.cs
@@ -1,45 +1,101 @@
-﻿Arrow arrow = new Arrow("steel", 60, "plastic");
+﻿ArrowHead arrowHead;
+ArrowFletching arrowFletching;
+int shaftLength = 0;
 
-Console.WriteLine("Choose the arrowhead [steel/wood/obsidian]: ");
-arrow._arrowhead = Console.ReadLine();
+Console.WriteLine("Choose the arrowhead:\n1 - Steel\n2 - Wood\n3 - Obsidian");
+Console.Write("> ");
+switch (Convert.ToInt32(Console.ReadLine()))
+{
+    case 1:
+        arrowHead = ArrowHead.Steel;
+        break;
+    case 2:
+        arrowHead = ArrowHead.Wood;
+        break;
+    case 3:
+        arrowHead = ArrowHead.Obsidian;
+        break;
+    default:
+        arrowHead = ArrowHead.Unknown;
+        break;
+}
 
-Console.WriteLine("Choose the shaft [plastic/turkey/goose]: ");
-arrow._fletching = Console.ReadLine();
+Console.WriteLine("Choose the shaft:\n1 - Plastic\n2 - Turkey Feathers\n3 - Goose Feathers");
+Console.Write("> ");
+switch (Convert.ToInt32(Console.ReadLine()))
+{
+    case 1:
+        arrowFletching = ArrowFletching.Plastic;
+        break;
+    case 2:
+        arrowFletching = ArrowFletching.TurkeyFeather;
+        break;
+    case 3:
+        arrowFletching = ArrowFletching.GooseFeather;
+        break;
+    default:
+        arrowFletching = ArrowFletching.Unknown;
+        break;
+}
 
-Console.WriteLine("Choose the shaft length [60-100cm]:");
-arrow._shaftLength = Convert.ToInt32(Console.ReadLine());
+while (shaftLength < 60 || shaftLength > 100)
+{
+    Console.WriteLine("Choose the shaft length [60-100cm]");
+    Console.Write("> ");
+    shaftLength = Convert.ToInt32(Console.ReadLine());
+}
 
-// TODO Remake into enums
+// Craft the arrow
+Arrow arrow = new Arrow(arrowHead, arrowFletching, shaftLength);
 
-Console.WriteLine($"The cost of the arrow is: {arrow.CalculateCost()}");
-
+// Calculate the cost
+if (arrowHead != ArrowHead.Unknown && arrowFletching != ArrowFletching.Unknown)
+    Console.WriteLine($"The cost of that arrow is: {arrow.CalculateCost()}");
+else
+    Console.WriteLine("Stop wasting my time with weird requests. I do NOT make arrows with recycled dog fur!");
 
 
 class Arrow
 {
-    public string _arrowhead;
+    public ArrowHead _arrowhead;
+    public ArrowFletching _fletching;
     public int _shaftLength;
-    public string _fletching;
     
-    public Arrow(string arrowhead, int shaftLength, string fletching)
+    public Arrow(ArrowHead arrowhead, ArrowFletching fletching, int shaftLength)
     {
         _arrowhead = arrowhead;
         _shaftLength = shaftLength;
         _fletching = fletching;
     }
     
-    // Calculate price TODO Add cases for each material and length
     public decimal CalculateCost()
     {
         decimal cost = 0;
 
-        if (_arrowhead == "steel")
+        // Add the cost of the arrowhead
+        if (_arrowhead == ArrowHead.Steel)
             cost += 10;
-        if (_shaftLength == 60)
-            cost += _shaftLength * 0.05m;
-        if (_fletching == "plastic")
+        else if (_arrowhead == ArrowHead.Wood)
+            cost += 3;
+        else if (_arrowhead == ArrowHead.Obsidian)
+            cost += 5;
+        else
+            cost += 0;
+        
+        // Add the cost of the fletching
+        if (_fletching == ArrowFletching.Plastic)
             cost += 10;
+        if (_fletching == ArrowFletching.TurkeyFeather)
+            cost += 5;
+        if (_fletching == ArrowFletching.GooseFeather)
+            cost += 3;
+        
+        // Add the cost of shaft by length
+        cost += _shaftLength * 0.05m;
         
         return cost;
     }
 }
+
+enum ArrowHead {Unknown, Steel, Wood, Obsidian};
+enum ArrowFletching {Unknown, Plastic, TurkeyFeather, GooseFeather};
